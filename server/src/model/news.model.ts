4b9421af66f9f68f4061e4ca6e2f9d59171eae3c
Karanjot Singh <k.jsingh.12003@gmail.com>
2025-05-04 09:19:12 +0530

    Aptos savepoint

--- server/src/model/news.model.ts
+++ server/src/model/news.model.ts
@@ -4,7 +4,13 @@ const newsSchema = new Schema({
     title: { type: String, required: [true, "Please enter title"] },
     description: { type: String, required: [true, "Please enter description"] },
     thumbnail: { type: String },
-    files: [{ type: String, url: String }]
+    files: [{ type: String, url: String }],
+    category: { type: String, enum: ["Sensitive", "Emergency", "Solution", "Unknown"] },
+    sub_category: { type: String, enum: ["Verified", "Potential Flagged", "Trending", "Unknown"] },
+    Labels: [{ type: String }],
+    score: { type: Number },
+    score_reasoning: [{ type: String }],
+    insights: String
 }, { timestamps: true })
 
 export const News = model("News", newsSchema);
--- server/src/router/news.router.ts
+++ server/src/router/news.router.ts
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import { createNews } from "../news.controller.js";
+import { createNews, getNews } from "../controller/news.controller.js";
 import { upload } from "../middlewares/upload.js";
 
 const newsRouter = Router();
 
-newsRouter.post("/", upload.fields([
-    { name: "thumbnail", maxCount: 1 },
-    { name: "files", maxCount: 10 }
-]), createNews);
+newsRouter.post("/", upload.fields([{ name: "thumbnail", maxCount: 1 }, { name: "files", maxCount: 10 }]), createNews);
 
-newsRouter.get("/", (req, res) => {
-    res.json({ msg: "News route is working" });
-});
+newsRouter.get("/", getNews);
 
 
 export default newsRouter;
