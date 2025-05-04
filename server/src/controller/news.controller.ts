72a70b2f32133caa809cc1e7d5ba9a1426b7d4f9
Karanjot Singh <k.jsingh.12003@gmail.com>
2025-05-04 11:04:46 +0530

    Backend Complete

--- server/src/controller/news.controller.ts
+++ server/src/controller/news.controller.ts
@@ -57,82 +57,107 @@ export const createNews = catchAsync(async (req: Request, res: Response) => {
 
     // LLM handling
 
-    
+    const formData = new FormData();
+    formData.append("title", newsData.title);
+    formData.append("description", newsData.description);
+
+    if (files.thumbnail && files.thumbnail.length > 0) {
+        const thumbFile = files.thumbnail[0];
+        const thumbFileToUpload = new File([thumbFile.buffer], thumbFile.originalname, { type: thumbFile.mimetype });
+        formData.append('thumbnail', thumbFileToUpload as any);
+    }
+
+    if (files.files && Array.isArray(files.files) && files.files.length > 0) {
+        files.files.forEach((file, index) => {
+            const fileToUpload = new File([file.buffer], file.originalname, { type: file.mimetype });
+            formData.append(`files[${index}]`, fileToUpload as any);
+        });
+    }
+
+    let llmResponse;
+
+    const response = await fetch('http://127.0.0.1:5000/news/insights', {
+        method: 'POST',
+        body: formData
+    });
+
+    llmResponse = await response.json();
+
+    if (llmResponse.status === "false")
+        throw new AppError(llmResponse.error ?? "Failed to create news", 404)
+
 
     // Aptos Handling
 
-    // const privateKeyHex = process.env.APTOS_PRIVATE_KEY;
-    // const privateKeyBytes = Buffer.from(privateKeyHex.replace('0x', ''), 'hex');
-    // const privateKey = new Ed25519PrivateKey(privateKeyBytes);
+    const privateKeyHex = process.env.APTOS_PRIVATE_KEY;
+    const privateKeyBytes = Buffer.from(privateKeyHex.replace('0x', ''), 'hex');
+    const privateKey = new Ed25519PrivateKey(privateKeyBytes);
 
-    // let account;
-    // try {
-    //     account = Account.fromPrivateKey({ privateKey });
-    // } catch (error) {
-    //     throw new AppError("Invalid Aptos private key", 500);
-    // }
+    let account;
+    try {
+        account = Account.fromPrivateKey({ privateKey });
+    } catch (error) {
+        throw new AppError("Invalid Aptos private key", 500);
+    }
 
-    // const transactionToSimulate = await aptos.transaction.build.simple({
-    //     sender: account.accountAddress,
-    //     data: {
-    //         function: `${MODULE_ADDRESS}::${MODULE_NAME}::create_news`,
-    //         typeArguments: [],
-    //         functionArguments: [newsData.title, newsData.description, newsData.files, newsData.thumbnail || null,],
-    //     },
-    // });
+    const transactionToSimulate = await aptos.transaction.build.simple({
+        sender: account.accountAddress,
+        data: {
+            function: `${MODULE_ADDRESS}::${MODULE_NAME}::create_news`,
+            typeArguments: [],
+            functionArguments: [newsData.title, newsData.description, newsData.files, newsData.thumbnail || null,],
+        },
+    });
 
-    // const [simulation] = await aptos.transaction.simulate.simple({
-    //     signerPublicKey: account.publicKey,
-    //     transaction: transactionToSimulate,
-    // });
+    const [simulation] = await aptos.transaction.simulate.simple({
+        signerPublicKey: account.publicKey,
+        transaction: transactionToSimulate,
+    });
 
-    // if (!simulation) {
-    //     throw new AppError("Simulation result is empty", 500);
-    // }
+    if (!simulation) {
+        throw new AppError("Simulation result is empty", 500);
+    }
 
-    // if (!simulation.success) {
-    //     const errorMessage = simulation.vm_status.includes("EINSUFFICIENT_BALANCE") ? "Insufficient account balance for transaction" : `Transaction simulation failed: ${simulation.vm_status}`;
-    //     throw new AppError(errorMessage, 500);
-    // }
+    if (!simulation.success) {
+        const errorMessage = simulation.vm_status.includes("EINSUFFICIENT_BALANCE") ? "Insufficient account balance for transaction" : `Transaction simulation failed: ${simulation.vm_status}`;
+        throw new AppError(errorMessage, 500);
+    }
 
-    // const gasUsed = parseInt(simulation.gas_used, 10);
+    const gasUsed = parseInt(simulation.gas_used, 10);
 
-    // if (isNaN(gasUsed)) {
-    //     throw new AppError("Invalid gas_used value in simulation result", 500);
-    // }
+    if (isNaN(gasUsed)) {
+        throw new AppError("Invalid gas_used value in simulation result", 500);
+    }
 
-    // const maxGasAmount = Math.ceil(gasUsed * 1.2);
+    const maxGasAmount = Math.ceil(gasUsed * 1.2);
 
-    // const gasPrice = await aptos.getGasPriceEstimation();
+    const gasPrice = await aptos.getGasPriceEstimation();
 
-    // const finalTransaction = await aptos.transaction.build.simple({
-    //     sender: account.accountAddress,
-    //     data: {
-    //         function: `${MODULE_ADDRESS}::${MODULE_NAME}::create_news`,
-    //         typeArguments: [],
-    //         functionArguments: [newsData.title, newsData.description, newsData.files, newsData.thumbnail || null,],
-    //     },
-    //     options: { maxGasAmount, gasUnitPrice: gasPrice.gas_estimate, },
-    // });
+    const finalTransaction = await aptos.transaction.build.simple({
+        sender: account.accountAddress,
+        data: {
+            function: `${MODULE_ADDRESS}::${MODULE_NAME}::create_news`,
+            typeArguments: [],
+            functionArguments: [newsData.title, newsData.description, newsData.files, newsData.thumbnail || null,],
+        },
+        options: { maxGasAmount, gasUnitPrice: gasPrice.gas_estimate, },
+    });
 
-    // const pendingTransaction = await aptos.signAndSubmitTransaction({ signer: account, transaction: finalTransaction, });
+    const pendingTransaction = await aptos.signAndSubmitTransaction({ signer: account, transaction: finalTransaction, });
 
-    // const result = await aptos.waitForTransaction({ transactionHash: pendingTransaction.hash });
+    const result = await aptos.waitForTransaction({ transactionHash: pendingTransaction.hash });
 
-    // if (!result.success) {
-    //     throw new AppError(`Transaction failed: ${result.vm_status}`, 500);
-    // }
+    if (!result.success) {
+        throw new AppError(`Transaction failed: ${result.vm_status}`, 500);
+    }
 
     // MondoDB handling
 
-    const news = new News(newsData);
+    const news = new News({ ...newsData, ...llmResponse });
 
     await news.save();
 
-    res.status(201).json({
-        success: true, message: "News created successfully", news,
-        // transactionHash: pendingTransaction.hash,
-    });
+    res.status(201).json({ success: true, message: "News created successfully", news, txnhash: `https://explorer.aptoslabs.com/txn/${pendingTransaction.hash}/payload?network=testnet` });
 });
 
 export const getNews = catchAsync(async (_req: Request, res: Response) => {
